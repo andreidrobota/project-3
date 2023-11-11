cf34be36f3043f0bfe998283c676cd0c4cd452ea
andreidrobota <>
2023-11-11 05:23:53 +0000

    Create the function to generate the random 6 numbers between 1 and 49

--- run.py
+++ run.py
@@ -4,6 +4,19 @@ import random
 
 class Lotto:
     """ Lotto class"""
+    def __init__(self):
+        self.lotto_numbers = []
+    
+    def generate_numbers(self):
+        """
+        Generates six random numbers from 1 to 49
+        Returns the numbers in a list
+        """
+        self.lotto_numbers = random.sample(range(1, 50), 6)
+
+        return self.lotto_numbers
+    
+    
 
 
 def lotto_game():
